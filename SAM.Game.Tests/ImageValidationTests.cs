ab5bc28d634f4bbc98779c9c3ce920b0dc6cd0e4
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-08-15 10:05:34 +0800

    Validate image data and reject invalid images

---
+++ SAM.Game.Tests/ImageValidationTests.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Drawing;
+using System.IO;
+using Xunit;
+
+public class ImageValidationTests
+{
+    [Fact]
+    public void CorruptedImageIsRejected()
+    {
+        byte[] data = new byte[] { 0x00, 0x01, 0x02, 0x03 };
+        using var stream = new MemoryStream(data);
+
+        Bitmap? bitmap = null;
+        try
+        {
+            using var image = Image.FromStream(stream, useEmbeddedColorManagement: false, validateImageData: true);
+            bitmap = new Bitmap(image);
+        }
+        catch (ArgumentException)
+        {
+            // expected
+        }
+        catch (OutOfMemoryException)
+        {
+            // expected
+        }
+
+        Assert.Null(bitmap);
+    }
+}
